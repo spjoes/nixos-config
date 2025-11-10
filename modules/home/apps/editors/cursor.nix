9cbe10e02fe8a38200b97c4e5ccf3402eb337eb1
IAmThePerson <34802651+spjoes@users.noreply.github.com>
2025-11-09 20:10:28 -0800

    declare cursor user settings

--- modules/home/apps/editors/cursor.nix
+++ modules/home/apps/editors/cursor.nix
@@ -5,11 +5,20 @@
   programs.vscode = {
     enable = true;
     package = pkgs.code-cursor;
-    extensions = with pkgs.vscode-extensions; [
-      eamodio.gitlens # GitLens
-      jnoortheen.nix-ide # Nix IDE
-      wakatime.vscode-wakatime # Wakatime
-      # coderabbit.coderabbit-vscode # need to make a pr for this
-    ];
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        eamodio.gitlens # GitLens
+        jnoortheen.nix-ide # Nix IDE
+        wakatime.vscode-wakatime # Wakatime
+        # coderabbit.coderabbit-vscode # need to make a pr for this
+      ];
+      userSettings = {
+        # Sidebar
+        "workbench.activityBar.orientation" = "vertical";
+
+        # Extensions
+        "redhat.telemetry.enabled" = false;
+      };
+    };
   };
 }
