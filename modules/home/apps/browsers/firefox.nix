71823f7bddd3230bb3dd6ace26428eba4aee5e59
IAmThePerson <34802651+spjoes@users.noreply.github.com>
2025-11-10 16:10:13 -0800

    Update firefox.nix

--- modules/home/apps/browsers/firefox.nix
+++ modules/home/apps/browsers/firefox.nix
@@ -110,7 +110,7 @@ in
           settings = [
             {
               name = "Get unix";
-              url = "javascript:(function(){function parseIsoDatetime(dtstr) {     return new Date(dtstr); };  startElement = document.getElementById('availability-time'); endElement = document.getElementById('availability-time2');  start = parseIsoDatetime(startElement.getAttribute('datetime')); end = parseIsoDatetime(endElement.getAttribute('datetime'));  para = document.createElement("p"); para.innerText = Math.floor(start.getTime() / 1000) + '-' + Math.ceil(end.getTime() / 1000);  endElement.parentElement.appendChild(para);})();";
+              url = "javascript:(function(){function parseIsoDatetime(dtstr) {     return new Date(dtstr); };  startElement = document.getElementById('availability-time'); endElement = document.getElementById('availability-time2');  start = parseIsoDatetime(startElement.getAttribute('datetime')); end = parseIsoDatetime(endElement.getAttribute('datetime'));  para = document.createElement('p'); para.innerText = Math.floor(start.getTime() / 1000) + '-' + Math.ceil(end.getTime() / 1000);  endElement.parentElement.appendChild(para);})();";
             }
             {
               name = "03070"; # Zip code for New Hampshire
