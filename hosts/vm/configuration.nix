5b463ca4af428ba19d834b03f6acd04191cd5d8b
IAmThePerson <34802651+spjoes@users.noreply.github.com>
2026-01-01 14:06:33 -0500

    update excluded packages

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -40,6 +40,12 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.desktopManager.plasma6.enable = true;
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    konsole
+    elisa
+    kate
+    okular
+  ];
 
   services.greetd = {
     enable = true;
