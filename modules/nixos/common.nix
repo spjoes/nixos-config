510f7b7d3351b6f3a59d955b851e6576e3cf1ab5
Joey <joeykerp@gmail.com>
2026-01-23 22:40:44 -0800

    updated timezone

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 {
   # Set your time zone.
-  time.timeZone = "America/New_York";
+  time.timeZone = "America/Los_Angeles";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
